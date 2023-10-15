799614c2a9aa30904d8ca2640abf5a07cf7a7408
BakaBaka1368 <phongtrando2004@gmail.com>
2023-10-15 22:33:53 +1030

    feat: finish validation

--- addtoshoppinglist.cpp
+++ addtoshoppinglist.cpp
@@ -56,7 +56,7 @@ void AddToShoppingList::on_buttonBox_accepted()
     query.exec();
     query.finish();
     query.clear();
-    QMessageBox::critical(this, "Success", "Added successfully!");
+    QMessageBox::information(this, "Success", "Added successfully!");
     qDebug() << "Last error: " << query.lastError().text();
     ui->noteLineEdit->clear();
     ui->onlineShoppingLinkLineEdit->clear();
@@ -67,6 +67,7 @@ void AddToShoppingList::on_buttonBox_accepted()
 
 bool AddToShoppingList::validateUserInput() {
     QString ingredientName = ui->ingredientComboBox->currentText();
+    QString phoneNumber = ui->phoneNumberLineEdit->text();
     // check if name already exist
     QSqlDatabase database = QSqlDatabase::database("DB0");
     QSqlQuery query(database);
@@ -81,6 +82,12 @@ bool AddToShoppingList::validateUserInput() {
             qDebug() << "Last error: " << query.lastError().text();
             return false;
         }
+    }
+    // check phone number digit
+    static QRegularExpression regex("^0[2-9][0-9]{8}$");
+    if (!regex.match(phoneNumber).hasMatch()) {
+        QMessageBox::critical(this, "Error", "Wrong phone number format!");
+        return false;
     }
     return true;
 }
