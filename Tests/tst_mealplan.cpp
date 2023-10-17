c5156161c15f8fbf2a1b46274a4aed975493a100
BakaBaka1368 <phongtrando2004@gmail.com>
2023-10-17 17:27:48 +1030

    Update tst_mealplan.cpp

--- Tests/tst_mealplan.cpp
+++ Tests/tst_mealplan.cpp
@@ -1,5 +1,6 @@
 #include "../mealplan.h"
-#include "D:\Games and Apps\Qt app\build-FoodManagementSystem-Desktop_Qt_6_6_0_MinGW_64_bit-Debug\FMS_autogen\include\ui_mealplan.h"
+//#include "D:\Games and Apps\Qt app\build-FoodManagementSystem-Desktop_Qt_6_6_0_MinGW_64_bit-Debug\FMS_autogen\include\ui_mealplan.h"
+#include "C:\Users\Legion\Documents\GitHub\build-FoodManagementSystem-Desktop_Qt_6_5_2_MinGW_64_bit-Debug\FoodManagementSystem_autogen\include\ui_mealplan.h"
 
 #include <QTest>
 
@@ -12,7 +13,9 @@ private:
 private slots:
     void initTestCase() {
         QSqlDatabase database = QSqlDatabase::addDatabase("QSQLITE", "DB0");
-        database.setDatabaseName("D:/Games and Apps/Qt app/FoodManagementSystem/FoodManagementSystem.db");
+//        database.setDatabaseName("D:/Games and Apps/Qt app/FoodManagementSystem/FoodManagementSystem.db");
+    database.setDatabaseName("C:/Users/Legion/Documents/GitHub/FoodManagementSystem/FoodManagementSystem.db");
+
     }
     void testShowMealPlan() {
         MealPlan mealplan;
